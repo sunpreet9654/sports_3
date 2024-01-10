8b100b31e343fe4d46d24250f7d69e21b2448e4b
sunpreet singh <sam850000@gmail.com>
2024-01-10 12:56:57 +0530

    first commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2024, 0, 20, 8)}); // year, month, date, hour
+        });
+});		
+
